4b582eafb9fb3c64d7309a57f429a8a0f9a58875
Dwi Wahyudi <dwi.wahyudi.14@gmail.com>
2020-08-09 17:45:33 +0700

    add mail sending demo at main function

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/dwahyudi/golang-smtp/util"
+)
+
+func main() {
+	simpleMailDemo()
+}
+
+func simpleMailDemo() {
+	mail := util.Mail{
+		From:       "no-reply@example00.com",
+		To:         []string{"user01@example99.com", "user02@example.com"},
+		Subject:    "Sample Subject",
+		BodyType:   "text/html",
+		Body:       "<html><body><p>Sample body</p></body></html>",
+		Attachment: []string{"temp/cat.jpg", "temp/orange.jpg"},
+	}
+
+	util.MailSend(mail)
+}
